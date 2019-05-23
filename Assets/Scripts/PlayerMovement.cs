31d9b6e9ae3f94d7657e1230c4707c0511d00e49
The3rdwheelr <48418427+The3rdwheelr@users.noreply.github.com>
2019-05-23 12:49:12 +1200

    Player movement added
    xd

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public void Update()
+    {
+        // sets the player to move left/right on key press
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+        }
+    }
+}
