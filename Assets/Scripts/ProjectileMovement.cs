005ae45fe330dfb1a92ad1af18db3bc9970d6560
The3rdwheelr <48418427+The3rdwheelr@users.noreply.github.com>
2019-05-24 15:06:50 +1200

    working on movement script for projectile

---
+++ Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    public float speed;
+    Vector3 pos;
+    Vector3 newPos;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        newPos = pos + (Vector3.down * speed);
+        transform.position = newPos;
+    }
+}
