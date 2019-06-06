731a8a03cd89366a307fe685e2ceb6afffcf59e2
The3rdwheelr <48418427+The3rdwheelr@users.noreply.github.com>
2019-06-07 11:11:52 +1200

    fixed player clamping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int lanesFromCentre;
+    float laneBorderMin;
+    float laneBorderMax;
+
+    private void Start()
+    {
+        laneBorderMin = transform.position.x - lanesFromCentre;
+        laneBorderMax = transform.position.x + lanesFromCentre;
+    }
+
     public void Update()
     {
         // sets the player to move left/right on key press
-        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -lanesFromCentre)
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > laneBorderMin)
         {
             transform.position += Vector3.left;
         }
-        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < lanesFromCentre)
+        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < laneBorderMax)
         {
             transform.position += Vector3.right;
         }
