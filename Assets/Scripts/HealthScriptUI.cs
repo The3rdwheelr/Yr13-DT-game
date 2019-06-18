4b2aeae01c8c5466ccacaf5dff0657046562df5b
The3rdwheelr <48418427+The3rdwheelr@users.noreply.github.com>
2019-06-18 14:25:52 +1200

    UI continuation

---
+++ Assets/Scripts/HealthScriptUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthScriptUI : MonoBehaviour
+{
+    public static int currentHearts;
+    public int oldHearts;
+
+    public Sprite[] heartTypes;
+    public Image[] heartSlots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHearts = 5;
+        oldHearts = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (oldHearts != currentHearts)
+        {
+            oldHearts = currentHearts;
+            HeartUIUpdate();
+        }
+    }
+
+    void HeartUIUpdate()
+    {
+        
+    }
+
+}
